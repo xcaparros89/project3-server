4d3d5d127dc2b0b65edcfec073a723148dd73c85
Xavier Caparros <xcaparros@hotmail.com>
2020-11-26 14:44:14 +0100

    sdfsdfds

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.023b8d50e05e661a8ed4aaca8e468d60.js"
+  "/precache-manifest.1332a9d7f7a215f64784bcb612e943ed.js"
 );
 
 self.addEventListener('message', (event) => {
